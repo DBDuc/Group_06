f65b07c5c2877c23d150e94e06ba4c1c87955ccb
Ralph <ralphiweber@hotmail.de>
2020-10-22 11:39:48 +0100

    Add working SQL Query

--- Backend/SQLImitation.py
+++ Backend/SQLImitation.py
@@ -3,27 +3,38 @@ import mysql.connector
 class SQL(object):
 
     def __init__(self):
-        mysql.connector.connect(host='127.0.0.1',
-                                port = "3306",
-                                database="mysql-server",
+        self.mydb = mysql.connector.connect(host='localhost',
+                                #port = "3306",
+                                database="db_grad_cs_1917",
                                 user="root",
                                 password = "ppp")
+        self.cur = self.mydb.cursor()
 
 
-    def Query(self,Query):
-        print(Query)
-        return None
+    def SetQuery(self,Query):
+        self.cur.execute(Query)
+        self.mydb.commit()
 
+    def GetQuery(self,Query):
+        self.cur.execute(Query)
+        return self.cur.fetchall()
 
+sql = SQL()
 
 def RequestID(table,column,string):
-    sql = SQL()
-    return sql.Query("SELECT "+column+
+    id = sql.GetQuery("SELECT "+table+"_id"+
     " FROM "+table+
     " WHERE "+table+"_name = '"+string+"'"
     )
 
+    if id == []:
+        return None
+    else:
+        return id[0][0]
+
+
 def SetNewRow(table,content):
+    
     column_key = ""
     column_value = ""
     for key,val in content.items():
@@ -36,11 +47,9 @@ def SetNewRow(table,content):
     column_key = "("+column_key[:-1]+")"
     column_value = "("+column_value[:-1]+")"
 
-    sql = SQL()
-
-    sql.Query("LOCK TABLES "+table+" WRITE;"+
-    "/*!40000 ALTER TABLE "+table+" DISABLE KEYS */;"+
-    "INSERT INTO "+table+" "+column_key+" VALUES "+column_value+
-    "/*!40000 ALTER TABLE "+table+" ENABLE KEYS */;"+
-    "UNLOCK TABLES;"
-    )
+    sql.SetQuery("LOCK TABLES "+table+" WRITE;")
+    sql.SetQuery("/*!40000 ALTER TABLE "+table+" DISABLE KEYS */;")
+    sql.SetQuery("INSERT INTO "+table+" "+column_key+" VALUES "+column_value+";")
+    sql.SetQuery("/*!40000 ALTER TABLE "+table+" ENABLE KEYS */;")
+    sql.SetQuery("UNLOCK TABLES;")
+    
