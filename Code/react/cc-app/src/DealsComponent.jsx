4b51aba2e888c30d59dbc385b2c29574d6766dd4
Duc <sdsu.duc@gmail.com>
2020-10-22 09:28:55 +0100

    read and parse deals_data

--- Code/react/cc-app/src/DealsComponent.jsx
+++ Code/react/cc-app/src/DealsComponent.jsx
@@ -5,28 +5,21 @@ function DealsComponent() {
 
   // Similar to componentDidMount and componentDidUpdate:
   useEffect(() => {
-    // Update the document title using the browser API
-    //document.title = `You clicked ${count} times`;
+    const dealsUrl = 'http://localhost:8090/json_example';
 
-    //const apiUrl = 'http://localhost:8090/client/testservice';
-    //fetch(apiUrl)
-    //  .then((response) => response.json())
-    //  .then((data) => console.log(deals));
-    const decoder = new TextDecoder('utf-8')
-    const dealsUrl = 'http://localhost:8090/deals';
-    //fetch(dealsUrl)
-    //  .then(response => {
-    //  response.body
-    //  .getReader()
-    //  .read()
-    //  .then(({value, done}) => {
-     //   setDeals([...deals, decoder.decode(value)]);
-      //  console.log(deals.length);
-      //})
-    //})
+    fetch(dealsUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      let arr = [];
+      data['deals'].forEach(element => {
 
+        arr.push([element['data']['instrumentName'],element['data']['cpty'],element['data']['price'], element['data']['type'],element['data']['quantity'],element['data']['time'],])
+      });
+      setDeals(arr);
+      console.log(data);
+    });
 
-  });
+  },[deals]);
 
   return (
     <div style={{overflow: "visible"}}>
